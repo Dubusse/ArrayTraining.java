6c2d8ed04b233ee7c4080d2b63454ce5d77f526e
Arthur Dubusse <arthur.dubusse@gmail.com>
2019-10-07 10:07:56 +0200

    exercice fait

--- src/exercices/MatrixTraining.java
+++ src/exercices/MatrixTraining.java
@@ -1,27 +1,28 @@
+​
 public class MatrixTraining {
-
+​
     /**
-     * @param lines   height of the matrix, eg: 3
+     * @param lines height of the matrix, eg: 3
      * @param columns width of the matrix, eg: 2
-     * @return an empty int matrix of lines and columns size,
-     * eg: {{0, 0}, {0, 0}, {0, 0}}
+     * @return an empty int matrix of lines and columns size, eg: {{0, 0}, {0,
+     * 0}, {0, 0}}
      */
     public int[][] emptyIntMatrix(int lines, int columns) {
-
-        return null;
+        int[][] matrix = new int[lines][columns];
+        return matrix;
     }
-
+​
     /**
-     * @param lines   height of the matrix, eg: 2
+     * @param lines height of the matrix, eg: 2
      * @param columns width of the matrix, eg: 3
-     * @return an empty string matrix of lines and columns size,
-     * eg: {{null, null, null}, {null, null, null}}
+     * @return an empty string matrix of lines and columns size, eg: {{null,
+     * null, null}, {null, null, null}}
      */
     public String[][] emptyStringMatrix(int lines, int columns) {
-
-        return null;
+        String[][] matrix = new String[lines][columns];
+        return matrix;
     }
-
+​
     /**
      * @param a first array
      * @param b second array
@@ -29,10 +30,10 @@ public class MatrixTraining {
      * @return an int matrix with a, b and c arrays
      */
     public int[][] intMatrix(int[] a, int[] b, int[] c) {
-
-        return null;
+        int[][] matrix = {a, b, c};
+        return matrix;
     }
-
+​
     /**
      * @param a first array
      * @param b second array
@@ -40,87 +41,121 @@ public class MatrixTraining {
      * @return a String matrix with a, b and c arrays
      */
     public String[][] stringMatrix(String[] a, String[] b, String[] c) {
-
-        return null;
+        String[][] matrix = {a, b, c};
+        return matrix;
+​
     }
-
+​
     /**
      * @param matrix, eg: {{3, 4}, {6, 7}, {5, 8}}
      * @return number of matrix lines, eg: 3
      */
     public int height(int[][] matrix) {
-
-        return 0;
+​
+        return matrix.length;
     }
-
+​
     /**
      * @param matrix, eg: {{3, 4}, {6, 7}, {5, 8}}
      * @return number of matrix columns, eg: 2
      */
     public int width(int[][] matrix) {
-
-        return 0;
+​
+        return matrix[0].length;
     }
-
+​
     /**
      * @param matrix, eg: {{3, 4, 5}, {6, 7, 8}}
-     * @param line,   eg: 1
+     * @param line, eg: 1
      * @param column, eg: 0
      * @return value in matrix at line and column, eg: 6
      */
     public int valueAtPosition(int[][] matrix, int line, int column) {
-
-        return 0;
+​
+        return matrix[line][column];
     }
-
+​
     /**
      * @param matrix, eg: {{1, 2, 3}, {8, 5, 6}}
-     * @param value,  eg: 4
-     * @param line,   eg: 1
+     * @param value, eg: 4
+     * @param line, eg: 1
      * @param column, eg: 0
-     * @return the matrix with the value replaced at line and column,
-     * eg: {{1, 2, 3}, {4, 5, 6}}
+     * @return the matrix with the value replaced at line and column, eg: {{1,
+     * 2, 3}, {4, 5, 6}}
      */
     public int[][] replace(int[][] matrix, int value, int line, int column) {
-
-        return null;
+        matrix[line][column] = value;
+        return matrix;
     }
-
+​
     /**
      * @param matrix, eg: {{1, 2, 3}, {4, 5, 6}}
      * @return sum of matrix values, eg: 21
      */
     public int sum(int[][] matrix) {
-
-        return 0;
+        int somme = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                somme = somme + matrix[i][j];
+            }
+​
+        }
+        return somme;
+​
     }
-
+​
     /**
      * @param matrix, eg: {{1, 2, 3}, {4, 5, 6}}
      * @param search, eg: 5
      * @return if matrix contains searched value, eg: true
      */
     public boolean contains(int[][] matrix, int search) {
-
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (search == matrix[i][j]) {
+                    return true;
+                }
+            }
+​
+        }
+​
         return false;
     }
-
+​
     /**
      * @param matrix, eg: {{1, 2, 3}, {4, 5, 6}}
      * @return how many even numbers are in matrix, eg: 3
      */
     public int countEvens(int[][] matrix) {
-
-        return 0;
+        int even = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] % 2 == 0) {
+                    even = even + 1;
+                }
+            }
+​
+        }
+        return even;
     }
-
+​
     /**
      * @param matrix, eg: {{'d', 'b', 'a'}, {'a', 'd', 'a'}}
      * @param search, eg: 'a'
      * @return the number of character occurrences in matrix, eg: 3
      */
     public int occurrences(char[][] matrix, char search) {
-
-        return 0;
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == search) {
+                    count++;
+                }
+​
+            }
+​
+        }
+        return count;
     }
 }
+
